70b18aa3a642a26904129b667663480428115876
Patrick Ashi <patrickpeko1@gmail.com>
2025-02-10 12:15:18 +0100

    new

--- eatwellproj/wsgi.py
+++ eatwellproj/wsgi.py
@@ -1,9 +1,6 @@
-import sys
 import os
+from django.core.wsgi import get_wsgi_application
 
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-sys.path.append('/opt/render/project/src/eatwellbe')
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eatwellproj.settings')
 
-from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
